f8974926fdb5123e7306213aea1fc6f2fa7c9949
Philip Andersson <philip.andersson.1991@hotmail.com>
2023-10-31 19:26:55 +0100

    added bot reply to chat

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { Reveal } from "./utils/Reveal";
 import Header from "./ContentElements/Header";
-import MessageGroup from "./utils/MessageGroup";
-import Message from "./utils/Message";
-import Links from "./ContentElements/Links";
-
+import MessageGroup from "./utils/messages/MessageGroup";
+import Message from "./utils/messages/Message";
 
 const OverlayMenu = () => {
   return (
@@ -13,30 +11,69 @@ const OverlayMenu = () => {
         <Header />
       </Reveal>
       <div className="overlay-grid">
-        <Reveal delayTime={0.5}>
+        <Reveal delayTime={1}>
           <MessageGroup>
-            <Message text="I'm an mobile-app developer in progress and is currently looking for an internship to develop my skills!" delay={1500}/>
-            <Message text="hopefully at your company. " delay={3500}/>
+            <Message
+              text="I'm an mobile-app developer in progress and is currently looking for an internship to develop my skills!"
+              delay={1500}
+            />
+            <Message text="hopefully at your company. " delay={3500} />
           </MessageGroup>
         </Reveal>
-        <div></div>
-        <div></div>
-        <Reveal>
-          <Links />
-        </Reveal> 
-        
-        <Reveal delayTime={6}>
-          <MessageGroup><Message text="What you will gain is a free worker for 10 weeks that is verry driven and eager to learn everything that involvs coding." delay={6500}/>
-            <Message text="I am not afraid to dig in to new things, infact i verry much enjoy doing so!" delay={10000}/>
-            <Message text="And also i thrive when given responsibility and see it as an oportunity to show that i'm reliable!" delay={12500}/>
+        <Reveal delayTime={4.5}>
+        <MessageGroup isReply={true}>
+            <Message
+              isReply={true}
+              text="I am not afraid to dig in to new things, infact i verry much enjoy doing so!"
+              delay={12500}
+            />
+            <Message
+              isReply={true}
+              text="Also, i thrive when given responsibility and see it as an oportunity to show i'm reliable!"
+              delay={13350}
+            />
+            <Message
+              isReply={true}
+              text="Also, i thrive when given responsibility and see it as an oportunity to show i'm reliable!"
+              delay={13750}
+            />
           </MessageGroup>
         </Reveal>
-        <div></div>
-        <div></div>
-        <Reveal>
-          <Links />
-        </Reveal> 
-        
+        <Reveal delayTime={9}>
+          <MessageGroup>
+            <Message
+              text="What you will gain is a free worker for 10 weeks that is verry driven and eager to learn everything that involvs coding."
+              delay={9500}
+            />
+            <Message
+              text="I am not afraid to dig in to new things, infact i verry much enjoy doing so!"
+              delay={10000}
+            />
+            <Message
+              text="Also, i thrive when given responsibility and see it as an oportunity to show i'm reliable!"
+              delay={11500}
+            />
+          </MessageGroup>
+        </Reveal>
+        <Reveal delayTime={12}>
+          <MessageGroup isReply={true}>
+            <Message
+              isReply={true}
+              text="I am not afraid to dig in to new things, infact i verry much enjoy doing so!"
+              delay={12500}
+            />
+            <Message
+              isReply={true}
+              text="Also, i thrive when given responsibility and see it as an oportunity to show i'm reliable!"
+              delay={13350}
+            />
+            <Message
+              isReply={true}
+              text="Also, i thrive when given responsibility and see it as an oportunity to show i'm reliable!"
+              delay={13750}
+            />
+          </MessageGroup>
+        </Reveal>
       </div>
     </div>
   );
