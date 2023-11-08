8e396f1c18df6195994e19eaf7fe5f9dd7501d44
Philip Andersson <philip.andersson.1991@hotmail.com>
2023-11-08 09:11:19 +0100

    fixing dialog

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -19,17 +19,17 @@ const OverlayMenu = () => {
                 text="I'm an mobile-app developer in progress and i am currently looking for an internship to develop my skills!"
                 delay={1500}
               />
-              <Message text="Hopefully at your company. " delay={3500} />
+              <Message text="Hopefully at your company. " delay={2500} />
             </MessageGroup>
           </FadeTop>
         </Reveal>
-        <Reveal delayTime={4.5}>
+        <Reveal delayTime={3.5}>
           <FadeTop>
             <MessageGroup isReply={true}>
               <Message
                 isReply={true}
                 text="Philip is studying his second year at Göteborgs folkuniversitet."
-                delay={5000}
+                delay={4000}
               />
               <Message
                 isReply={true}
@@ -38,46 +38,46 @@ const OverlayMenu = () => {
                     Link to the education program plan.
                   </a>
                 }
-                delay={6000}
+                delay={4500}
               />
             </MessageGroup>
           </FadeTop>
         </Reveal>
-        <Reveal delayTime={9}>
+        <Reveal delayTime={5.5}>
           <FadeTop>
             <MessageGroup>
               <Message
                 text="What you will gain is a free worker for 10 weeks that is verry driven and eager to learn everything that involvs coding."
-                delay={9500}
+                delay={6000}
               />
               <Message
                 text="I am not afraid to dig in to new things, infact i verry much enjoy doing so!"
-                delay={10000}
+                delay={6500}
               />
               <Message
                 text="Also, i thrive when given responsibility and see it as an oportunity to show i'm reliable!"
-                delay={11500}
+                delay={7000}
               />
             </MessageGroup>
           </FadeTop>
         </Reveal>
-        <Reveal delayTime={12}>
+        <Reveal delayTime={8}>
           <FadeTop>
             <MessageGroup isReply={true}>
               <Message
                 isReply={true}
                 text="Philip has been studying both iOS and Android development and been coding apps using Swift, Kotlin and Java."
-                delay={12500}
+                delay={8500}
               />
               <Message
                 isReply={true}
                 text="Lately he has been diving in to some backend and webdevelopment, using React And setting up Databases and API's with AWS services, this website was built with just that."
-                delay={13350}
+                delay={9350}
               />
               <Message
                 isReply={true}
                 text="Check out some of his github repos!"
-                delay={13350}
+                delay={10350}
               />
               <Message
                 isReply={true}
@@ -86,43 +86,43 @@ const OverlayMenu = () => {
                     https://github.com/philipAson
                   </a>
                 }
-                delay={13750}
+                delay={10750}
+              />
+            </MessageGroup>
+          </FadeTop>
+        </Reveal>
+        <Reveal delayTime={12}>
+          <FadeTop>
+            <MessageGroup>
+              <Message
+                text="It's supposed to be from week 49-7. But the period is flexible and we can schedule the internship a couple of weeks later if that suits you better."
+                delay={12500}
+              />
+              <Message
+                text="I live in central Skåne and would love to come by the office and introduce myself further!"
+                delay={13500}
               />
             </MessageGroup>
           </FadeTop>
         </Reveal>
         <Reveal delayTime={14}>
           <FadeTop>
-            <MessageGroup>
-              <Message
-                text="It's supposed to be from week 49-7. But the period is flexible and we can schedule the internship a couple of weeks later if that suits you better."
-                delay={14500}
-              />
-              <Message
-                text="I live in central Skåne and would love to come by the office and introduce myself further!"
-                delay={15500}
-              />
-            </MessageGroup>
-          </FadeTop>
-        </Reveal>
-        <Reveal delayTime={16}>
-          <FadeTop>
             <MessageGroup isReply={true}>
               <Message
                 isReply={true}
                 text="Yup! and here's his number and email!"
-                delay={16500}
+                delay={14500}
               />
-              <Message isReply={true} text="+46702599344" delay={13350} />
+              <Message isReply={true} text="+46702599344" delay={15350} />
               <Message
                 isReply={true}
                 text="philip.andersson.1991@hotmail.com"
-                delay={17000}
+                delay={16000}
               />
               <Message
                 isReply={true}
                 text="You can view and download his resumé in the upper right corner"
-                delay={17500}
+                delay={16500}
               />
             </MessageGroup>
           </FadeTop>
