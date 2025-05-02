b3474282acef341a4357907601bd146f27148811
Philip Andersson <philip.andersson.1991@hotmail.com>
2025-05-02 12:28:32 +0200

    adjustments

--- src/components/VideoBg.jsx
+++ src/components/VideoBg.jsx
@@ -2,23 +2,32 @@ import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom"; // Importera useLocation
 
 const VideoBg = () => {
-  const location = useLocation(); // Använd useLocation för att få aktuell ruttinfo
+  const location = useLocation();
   const [video, setVideo] = useState("");
 
   useEffect(() => {
-    // Uppdatera video baserat på routen
     const videoURL = location.pathname === "/about"
       ? "https://pippin-portfolio-bucket.s3.eu-north-1.amazonaws.com/Sequence+01.mp4"
       : "https://pippin-portfolio-bucket.s3.eu-north-1.amazonaws.com/vidBgSmall.mp4";
-    
+
     setVideo(videoURL);
-  }, [location]); // Lägg till location som en beroende till useEffect
+  }, [location]);
 
   return (
     <div className="bgContainer">
-      <video src={video} autoPlay loop muted playsInline />
+      <video
+        key={video} // Gör att videon laddas om korrekt när src ändras
+        src={video}
+        autoPlay
+        loop
+        muted
+        playsInline
+        preload="auto"
+        style={{ opacity: 0.8 }} // Visuell förbättring
+      />
     </div>
   );
 };
+
 
 export default VideoBg;
