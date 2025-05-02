d5a73757bc65451bd1ad9cec41be6b24822364f4
Philip Andersson <philip.andersson.1991@hotmail.com>
2025-05-02 12:52:14 +0200

    text adjustments

--- src/components/ContentElements/About.jsx
+++ src/components/ContentElements/About.jsx
@@ -1,12 +1,11 @@
 const About = () => {
     return (
         <div className="about-page">
-            Hello! im a software developer @ A North Group AB. 
-            I live in small town called Norra Rörum, Skåne together with my partner and our two
+            Hello! I'm a software developer @ A North Group AB. 
+            I live in Norra Rörum together with my partner and our two
             dogs. Outside of work, I’m passionate about spending time outdoors.
             Whether it's taking long walks in the woods, hunting, or
-            paragliding. Nature plays a big part in my life and inspires both my
-            personal and professional mindset: calm, focused, and always seeking
+            paragliding. Nature plays a big part in my life. calm, focused, and always seeking
             new heights.
         </div>
     );
