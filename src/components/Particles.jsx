b7f2311decacdf8f15660da775fc7b7bfd3566af
Philip Andersson <philip.andersson.1991@hotmail.com>
2025-05-04 22:04:16 +0200

    dot x particles

--- src/components/Particles.jsx
+++ src/components/Particles.jsx
@@ -1,22 +1,13 @@
+import React from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { useEffect, useMemo, useState } from "react";
-// import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
-// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
-// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
+import { loadAll } from "@tsparticles/all";
 
 const ParticlesComponent = (props) => {
     const [init, setInit] = useState(false);
-    // this should be run only once per application lifetime
     useEffect(() => {
         initParticlesEngine(async (engine) => {
-            // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-            // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-            // starting from v2 you can add only the features you need reducing the bundle size
-            //await loadAll(engine);
-            //await loadFull(engine);
-            await loadSlim(engine);
-            //await loadBasic(engine);
+            await loadAll(engine);
         }).then(() => {
             setInit(true);
         });
@@ -35,8 +26,22 @@ const ParticlesComponent = (props) => {
             },
             fpsLimit: 60,
             interactivity: {
-                detectsOn: "window",
+                detect_on: "canvas",
                 events: {
+                    onDiv: [
+                        {
+                            enable: true,
+                            selectors: ".contact-link",
+                            mode: "bounce",
+                            type: "rectangle",
+                        },
+                        {
+                            enable: true,
+                            selectors: ".dot",
+                            mode: "bounce",
+                            type: "rectangle",
+                        },
+                    ],
                     onClick: {
                         enable: true,
                         mode: "repulse",
@@ -47,12 +52,22 @@ const ParticlesComponent = (props) => {
                     },
                 },
                 modes: {
-                    push: {
-                        distance: 200,
-                        duration: 15,
-                    },
-                    grab: {
-                        distance: 150,
+                    modes: {
+                        push: {
+                            distance: 200,
+                            duration: 15,
+                        },
+                        grab: {
+                            distance: 150,
+                        },
+                        repulse: {
+                            distance: 250,
+                            duration: 1.2,
+                        },
+                        bounce: {
+                          distance: 250,
+                          duration: 1.2,
+                      },
                     },
                 },
             },
@@ -74,20 +89,23 @@ const ParticlesComponent = (props) => {
                         default: "bounce",
                     },
                     random: true,
-                    speed: 1,
+                    speed: 2,
                     straight: false,
                 },
                 number: {
                     density: {
                         enable: true,
                     },
-                    value: 250,
+                    value: 450,
                 },
                 opacity: {
                     value: 1.0,
                 },
                 shape: {
-                    type: "circle",
+                    type: "square",
+                    options: {
+                        sides: 5,
+                    },
                 },
                 size: {
                     value: { min: 1, max: 3 },
